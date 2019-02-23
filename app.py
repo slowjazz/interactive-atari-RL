d84080ae2f718aefd72a274d728cc68c6ad4d0f0
slowjazz <kenkraken@gmail.com>
2019-02-23 08:21:48 +0000

    add sliders and callbacks for frame,iteration

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
 import pandas as pd
 import plotly.graph_objs as go
 import plotly.plotly as py
@@ -10,14 +11,36 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-log_data = pd.read_csv("baby-a3c/breakout-v4/log-modelTest4-02-10-22-28.txt")
+log_data = pd.read_csv("baby-a3c/breakout-v4/log-model7-02-17-20-41.txt")
 log_data.columns = log_data.columns.str.replace(" ", "")
 
 app.layout = html.Div(children=[
-    html.H1(children='demo'),
-
-    html.Div(children=["Dash: A web application framework for Python",
-                       dcc.Graph(
+    html.H1(children='Interactive Atari RL'),
+    html.Div([
+        html.Div([
+            html.Div(id='frame-val'),
+            dcc.Slider(id='frame-slider',
+                   min = 0,
+                   max = 3000,
+                   value = 0,
+                   marks = {i: str(i) for i in range(0, 3000, 200)},
+                   step = None
+               
+                  )
+        ], style = {'padding-bottom':'50px'}),
+        html.Div([
+            html.Div(id='snapshot-val'),
+            dcc.Slider(id='snapshot-slider',
+                   min = 1,
+                   max = 100,
+                   value = 0,
+                   marks = {i: str(i) for i in range(1, 100, 5)},
+                   #step = None
+               
+                  )
+        ])
+    ], style={'padding-bottom':'20px'}),
+    html.Div(children=[dcc.Graph(
                            id='mean-epr_over_eps',
                            figure={
                                'data': [
@@ -56,6 +79,21 @@ app.layout = html.Div(children=[
             
 
 ])
+
+@app.callback(
+    Output(component_id='frame-val', component_property='children'),
+    [Input(component_id='frame-slider', component_property='value')]
+)
+def update_frame_slider(input_value):
+    return 'Frame number of episode: {}'.format(input_value)
+
+@app.callback(
+    Output(component_id='snapshot-val', component_property='children'),
+    [Input(component_id='snapshot-slider', component_property='value')]
+)
+def update_snapshot_slider(input_value):
+    return 'Model iteration (500k frame increments): {}'.format(input_value)
+
 
 if __name__ == '__main__':
     app.run_server(debug=True, host='0.0.0.0')

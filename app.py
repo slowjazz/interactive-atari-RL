8fa7b07ba9268685b2115bd33a6f5a910f725607
slowjazz <kenkraken@gmail.com>
2019-02-23 19:36:21 +0000

    added episode gen logging with h5

--- app.py
+++ app.py
@@ -33,8 +33,8 @@ app.layout = html.Div(children=[
             dcc.Slider(id='snapshot-slider',
                    min = 1,
                    max = 100,
-                   value = 0,
-                   marks = {i: str(i) for i in range(1, 100, 5)},
+                   value = 50,
+                   marks = {i: str(i) for i in range(0, 100, 5)},
                    #step = None
                
                   )
@@ -76,8 +76,7 @@ app.layout = html.Div(children=[
                        ]
            
              )
-            
-
+           
 ])
 
 @app.callback(

51fc1cec9f574ca553f3cac57108c6bd8854890a
Kenneth Li <kenneth.j.li@vanderbilt.edu>
2019-04-26 22:16:10 -0500

    Clean generate_data.py

--- visualize_atari/generate_data.py
+++ visualize_atari/generate_data.py
@@ -1,14 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # Jacobian vs. Perturbation
-# Visualizing and Understanding Atari Agents | Sam Greydanus | 2017 | MIT License
-
-# Arg scheme
-# 0 for only rollouts, 1 for only saliency, none for both
-
 from __future__ import print_function
-import warnings ; warnings.filterwarnings('ignore') # mute warnings, live dangerously
 
 import pandas as pd
 import h5py 
@@ -36,7 +29,7 @@ def get_rollout(model, seed = 1):
     return history
 
 
-
+# Some datafile to store data
 if 'model_rollouts_5_temp.h5' not in os.listdir('../static'):
     store = h5py.File('../static/model_rollouts_5_temp.h5','w')
 else:
@@ -47,7 +40,7 @@ else:
 exceptions = []
 episodes = 5 # episodes to replay per iteration
 iterations = [1,19,30,40,50,60,70,80,90,100] # iterations we want to look at
-#iterations=[30]
+
 
 def filter_iterations(x):
     return any('.'+str(i)+'.' in x for i in iterations)
@@ -81,8 +74,7 @@ if len(sys.argv) > 1 and sys.argv[1]==1:
     store.close()
     exit()
     
-from torch.multiprocessing import Pool
-CPU_COUNT = torch.multiprocessing.cpu_count()
+
 
 def saliency_on_atari_frame_short(saliency, atari, fudge_factor, channel=2, sigma=0):
     # sometimes saliency maps are a bit clearer if you blur them
@@ -92,48 +84,12 @@ def saliency_on_atari_frame_short(saliency, atari, fudge_factor, channel=2, sigm
     S = S if sigma == 0 else gaussian_filter(S, sigma=sigma)
     S -= S.min() ; S = fudge_factor*pmax * S / S.max()
     return S
-#     I = atari.astype('uint16')
-#     I[35:195,:,channel] += S.astype('uint16')
-#     I = I.clip(1,255).astype('uint8')
-#     return I
 
-# def run_through_model(model, ins_instance, hx_instance, interp_func=None, mask=None, blur_memory=None, mode='actor'):
-#     if mask is None:
-#         im = prepro(ins_instance)
-#     else:
-#         assert(interp_func is not None, "interp func cannot be none")
-#         im = interp_func(prepro(ins_instance).squeeze(), mask).reshape(1,80,80) # perturb input I -> I'
-#     tens_state = torch.Tensor(im)
-#     state = Variable(tens_state.unsqueeze(0), volatile=True)
-#     hx = Variable(torch.Tensor(hx_instance).view(1,-1))
-#     #hx = Variable(torch.Tensor(history['hx'][ix-1]).view(1,-1))
-#     #cx = Variable(torch.Tensor(history['cx'][ix-1]).view(1,-1))
-#     #if blur_memory is not None: cx.mul_(1-blur_memory) # perturb memory vector
-#     #return model((state, (hx, cx)))[0] if mode == 'critic' else model((state, (hx, cx)))[1]
-#     return model((state, hx))[0] if mode == 'critic' else model((state, hx))[1]
 
-# def score_frame(model, ins_instance, hx_instance, r, d, interp_func, mode='actor'):
-#     # r: radius of blur
-#     # d: density of scores (if d==1, then get a score for every pixel...
-#     #    if d==2 then every other, which is 25% of total pixels for a 2D image)
-#     assert mode in ['actor', 'critic'], 'mode must be either "actor" or "critic"'
-#     L = run_through_model(model, ins_instance, hx_instance, interp_func, mask=None, mode=mode)
-#     scores = np.zeros((int(80/d)+1,int(80/d)+1)) # saliency scores S(t,i,j)
-#     for i in range(0,80,d):
-#         for j in range(0,80,d):
-#             mask = get_mask(center=[i,j], size=[80,80], r=r)
-#             l = run_through_model(model, ins_instance, hx_instance, interp_func, mask=mask, mode=mode)
-#             #scores[int(i/d),int(j/d)] = (L-l).pow(2).sum().mul_(.5).data[0]
-#             scores[int(i/d),int(j/d)] = (L-l).pow(2).sum().mul_(.5).item()
-#     pmax = scores.max()
-#     scores = imresize(scores, size=[80,80], interp='bilinear').astype(np.float32)
-#     print('done scoring frame')
-#     return pmax * scores / scores.max()
+from torch.multiprocessing import Pool
+CPU_COUNT = torch.multiprocessing.cpu_count()
 
-# prepro = lambda img: imresize(img[35:195].mean(2), (80,80)).astype(np.float32).reshape(1,80,80)/255.
-# searchlight = lambda I, mask: I*mask + gaussian_filter(I, sigma=3)*(1-mask) # choose an area NOT to blur
-# occlude = lambda I, mask: I*(1-mask) + gaussian_filter(I, sigma=3)*mask # choose an area to blur
-
+# uncomment commented-out lines later to use multi-processing (but in my experience this performed worse than single-processing)
 
 def get_saliency_mp(model, ins_instance, hx_instance):
     radius = 5
